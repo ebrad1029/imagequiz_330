1774e14d5faf54e4bac457d7cf07efb9e3beb9bf
ebrad1029 <60163679+ebrad1029@users.noreply.github.com>
2020-05-03 18:54:01 -0700

    Final Draft of Quiz

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
-function answer(){
+function quiz_1(){
   var q1 = document.quiz.q1.value;
+  var q2 = document.quiz.q2.value;
+  var q3 = document.quiz.q3.value;
   var correct = 0;
+
     if (q1 == 'Elephant') {
       correct++;
     }
-  //document.getElementById('after_submit').style.visibiltiy='visible';
-  document.getElementById('number_correct').innerHTML = "you got " + correct +
+    if (q2 == 'Deer') {
+      correct++;
+    }
+    if(q3 == 'Dog') {
+      correct++;
+    }
+
+
+  document.getElementById('number_correct').innerHTML = "You got " + correct +
     ' correct.';
 
+}
 
+function quiz_2(){
+  var q1 = document.quiz.q1.value;
+  var q2 = document.quiz.q2.value;
+  var q3 = document.quiz.q3.value;
+  var correct = 0;
 
+    if (q1 == 'Burger') {
+      correct++;
+    }
+    if (q2 == 'Pasta') {
+      correct++;
+    }
+    if(q3 == 'Orange') {
+      correct++;
+    }
+
+  document.getElementById('number_correct').innerHTML = "You got " + correct +
+    ' correct.';
 
 }
